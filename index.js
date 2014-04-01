82bd7c135def5e9a3ce98dd181e0e7609200ac4b
Gregg Caines <gregg@caines.ca>
2014-03-31 19:34:52 -0700

    version bump 0.2.1 use istanbul for coverage.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var dir = './lib/';
-if (process.env.urlgreyConnect_COVERAGE){
-  dir = './lib-cov/';
-}
-module.exports = require(dir + 'urlgrey-connect');
+var urlgrey = require('urlgrey');
+var isFunction = function(functionToCheck) {
+ var getType = {};
+ return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+};
+
+module.exports = function(protocol, name){
+	if (!name){
+		name = 'uri';
+	}
+	return function(req, res, next){
+    var protocolValue = protocol;
+    if (isFunction(protocol)){
+      protocolValue = protocol(req);
+    }
+    var url = protocolValue + '://' + req.headers.host + req.url;
+		req[name] = urlgrey(url);
+		next();
+	};
+};
